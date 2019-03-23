e13b16b1d533b01bd445603ee5c80df5509b2fcb
Louise Norris <louisenorris73@yahoo.co.uk>
2019-03-23 21:55:17 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,18 +1,16 @@
 function basicTeenager(age) {
-if (age >= 13 && age <= 19) {
-return "You are a teenager!"
-}
+if (age >=13 && age <=19) {return "You are a teenager!"}
 }
 
 function teenager(age) {
-  if (age >= 13 && <= 19)
+  if (age >= 13 && age <= 19)
   return `"You are a teenager!"`
 } else {
   return `"You are not a teenager"`
 }
 
 function ageChecker(age) {
-  if (age >= 13 && <= 19)
+  if (age >= 13 && age <= 19)
   return `"You are a teenager!"`
 } else if (age >= 12) {
   return `"You are a kid"`
